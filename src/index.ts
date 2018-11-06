34ae9f26f52d2e056e38de99f59f83e2454d09da
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2018-11-06 13:09:59 -0800

    probot app with code to comment on new/reopened PRs

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Application } from "probot"; // eslint-disable-line no-unused-vars
+
+import { PullRequestsCreateResponse } from "@octokit/rest";
+
+export = (app: Application) => {
+  app.log("app loaded");
+  app.on(["pull_request.opened", "pull_request.reopened"], async context => {
+    const prContent = context.payload
+      .pull_request as PullRequestsCreateResponse;
+    app.log("PR opened/reopened");
+
+    const prFromOwnerRepo = prContent.head.repo.full_name;
+    const prFromRef = prContent.head.ref;
+    app.log(`repo: ${prFromOwnerRepo}, branch: ${prFromRef}`);
+    const prComment = context.issue({
+      body: `If you're a MSFT employee, click [this link](https://portal.azure-devex-tools.com/app/branch/${prFromOwnerRepo}/${prFromRef}) 
+      to view this PR's validation status on our new OpenAPI Hub spec management tool.`
+    });
+    await context.github.issues.createComment(prComment);
+  });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
