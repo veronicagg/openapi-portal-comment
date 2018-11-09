8dbe3c85b7d77050512e51d9de516e63d6f1984e
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2018-11-08 16:48:39 -0800

    add source query param

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export = (app: Application) => {
     const prFromRef = prContent.head.ref;
     app.log(`repo: ${prFromOwnerRepo}, branch: ${prFromRef}`);
     const prComment = context.issue({
-      body: `If you're a MSFT employee, click [this link](https://portal.azure-devex-tools.com/app/branch/${prFromOwnerRepo}/${prFromRef}) 
+      body: `If you're a MSFT employee, click [this link](https://portal.azure-devex-tools.com/app/branch/${prFromOwnerRepo}/${prFromRef}?source=github)
       to view this PR's validation status on our new OpenAPI Hub spec management tool.`
     });
     await context.github.issues.createComment(prComment);
