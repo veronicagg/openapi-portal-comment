45ae4a592187caaa6fd08ccb70cd2c500dde4775
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2018-11-16 17:29:21 -0800

    adding condition to only post comment if PR is against'master

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,17 @@ export = (app: Application) => {
     const prContent = context.payload.pull_request as PullRequestsCreateResponse
     app.log('PR opened/reopened')
 
-    const prFromOwnerRepo = prContent.head.repo.full_name
-    const prFromRef = prContent.head.ref
-    app.log(`repo: ${prFromOwnerRepo}, branch: ${prFromRef}`)
-    const prComment = context.issue({
-      body: `If you're a MSFT employee, click [this link](https://portal.azure-devex-tools.com/app/branch/${prFromOwnerRepo}/${prFromRef}?source=github)
+    if (prContent.base.ref === 'master') {
+      app.log('PR against master')
+      const prFromOwnerRepo = prContent.head.repo.full_name
+      const prFromRef = prContent.head.ref
+      app.log(`repo: ${prFromOwnerRepo}, branch: ${prFromRef}`)
+      const prComment = context.issue({
+        body: `If you're a MSFT employee, click [this link](https://portal.azure-devex-tools.com/app/branch/${prFromOwnerRepo}/${prFromRef}?source=github)
       to view this PR's validation status on our new OpenAPI Hub spec management tool.`
-    })
-    await context.github.issues.createComment(prComment)
+      })
+      await context.github.issues.createComment(prComment)
+    }
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
