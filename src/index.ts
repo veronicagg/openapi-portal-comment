26352a4047cb82a8cb385df3cd217ab4c3f40b76
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2018-12-11 14:43:57 -0800

    Revert ""Bot filters out comments for PRs with multiple versions/RPs"""

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,14 @@
-import { PullRequestsCreateResponse, PullRequestsGetFilesResponseItem } from '@octokit/rest'
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
-import { PartialSwagger, SwaggerPathScraper } from './github/SwaggerPathScraper'
+
+import { PullRequestsCreateResponse } from '@octokit/rest'
 
 export = (app: Application) => {
   app.log('app loaded')
-  const swaggerPathScraper = new SwaggerPathScraper()
-  let hasMultiVersions = false
   app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
     const prContent = context.payload.pull_request as PullRequestsCreateResponse
+    app.log('PR opened/reopened')
 
-    if (prContent !== null) {
-      app.log(
-        `PR opened/reopened for PR ${prContent.number} on repo ${prContent.head.repo.owner.login}/${
-          prContent.head.repo.name
-        }`
-      )
-      const prFiles = await context.github.pullRequests.getFiles({
-        number: prContent.number,
-        owner: prContent.head.repo.owner.login,
-        repo: prContent.head.repo.name
-      })
-      // below checks to make sure there is a spec file that matches our portal requirements:
-      //    Files must only update a single RP and a single version
-      //    Files must be using a valid path that is parsable to obtain the RP and version information
-      hasMultiVersions = checkForInvalidVersions(prFiles.data)
-    }
-
-    if (prContent.base.ref === 'master' && !hasMultiVersions) {
+    if (prContent.base.ref === 'master') {
       app.log('PR against master')
       const prFromOwnerRepo = prContent.head.repo.full_name
       const prFromRef = encodeURIComponent(prContent.head.ref)
@@ -38,48 +20,6 @@ export = (app: Application) => {
       await context.github.issues.createComment(prComment)
     }
   })
-
-  const checkForInvalidVersions = (entries: PullRequestsGetFilesResponseItem[]): boolean => {
-    app.log('getting files')
-    let firstVersion: PartialSwagger | undefined
-    let isInvalid: boolean = false
-
-    entries.forEach(element => {
-      if (isInvalid === false) {
-        app.log(`file found for path: ${element.contents_url}`)
-        const partialSwagger = swaggerPathScraper.parsePathToSwagger(element.contents_url)
-        if (partialSwagger !== undefined) {
-          if (firstVersion !== undefined) {
-            if (
-              partialSwagger.rpName !== firstVersion.rpName ||
-              partialSwagger.version !== firstVersion.version
-            ) {
-              // more than one RP or version was detected.  Jump out of loop and return true
-              app.log(`multiple versions or RPs detected with ${element.contents_url}`)
-              isInvalid = true
-            }
-          }
-          if (firstVersion === undefined) {
-            firstVersion = partialSwagger
-            app.log(`a valid swagger has been found for ${element.contents_url}`)
-          }
-
-          app.log(
-            `swagger file found for version: ${partialSwagger.version} name: ${
-              partialSwagger.rpName
-            }`
-          )
-        }
-      }
-      // no files were found that could be parsed
-      if (firstVersion === undefined) {
-        app.log(`no valid folder structures found within the files in the PR`)
-        isInvalid = true
-      }
-    })
-    return isInvalid
-  }
-
   // For more information on building apps:
   // https://probot.github.io/docs/
 
